e158a53e0a92174eec383425e3830995154e2572
masato-210831 <b86500b@gmail.com>
2024-06-02 08:50:55 +0900

    finished chatgpt_udemy_lecture

--- init_sqlite.py
+++ init_sqlite.py
@@ -2,6 +2,7 @@ import glob
 import os
 import subprocess
 
+
 CSV_DIR = "100knocks-preprocess/docker/work/data"
 DB_FILE = "sample.db"
 
@@ -16,5 +17,5 @@ for csv_file_path in csv_file_paths:
   table = basename.rstrip(".csv")
   
   print(f"loading..csv_file_path={csv_file_path}")
-  subprocess.run(["sqlite3", DB_FILE, f".import {csv_file_path} {table}"])
+  subprocess.run(["sqlite3","-separator", ",", DB_FILE, f".import {csv_file_path} {table}"])
   subprocess.run(["sqlite3", DB_FILE, f"select count(*) from {table}"])
